ed056a59ef33abd98f495c9315949a438569f75c
Verminas <veremeeva96@gmail.com>
2024-04-17 21:36:45 +0300

    feat: add my first react card-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,101 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled, {css} from 'styled-components'
+import {content} from './components/content'
+
+type ButtonPropsType = {
+  backColored: boolean
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Image src={content.card.image}/>
+        <Title>{content.card.title}</Title>
+        <Text>{content.card.text}</Text>
+        <WrapperButtons>
+          <Button backColored={true}>See more</Button>
+          <Button backColored={false}>Save</Button>
+        </WrapperButtons>
+      </Container>
     </div>
   );
 }
 
 export default App;
+
+
+const Container = styled.div`
+  max-width: 280px;
+  border-radius: 15px;
+  background-color: #FFFFFF;
+  box-shadow: 0px 4px 20px 5px #0000001A;
+  padding: 10px 10px 20px 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+`
+
+const Image = styled.img`
+  width: 280px;
+  height: 170px;
+  border-radius: 10px;
+`
+
+const Title = styled.h1`
+  font-family: Inter;
+  font-size: 16px;
+  font-weight: 700;
+  text-align: left;
+  color: #000000;
+`
+
+const Text = styled.p`
+  font-family: Inter;
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 20px;
+  text-align: left;
+  color: #ABB3BA;
+`
+
+const WrapperButtons = styled.div`
+  display: flex;
+  justify-content: start;
+  gap: 10px;
+`
+
+const Button = styled.button<ButtonPropsType>`
+  width: 86px;
+  height: 30px;
+  border-radius: 5px;
+  text-align: center;
+  border: 2px solid #4E71FE;
+  
+  font-family: Inter;
+  font-size: 10px;
+  font-weight: 700;
+  line-height: 20px;
+  
+  background-color: #FFFFFF;
+  color: #4E71FE;
+  
+  &:hover{
+    background-color: #4E71FE;
+    color: #FFFFFF;
+  }
+  
+  ${props => props.backColored && css<ButtonPropsType>`
+    background-color: #4E71FE;
+    color: #FFFFFF;
+    
+    &:hover{
+      background-color: #FFFFFF;
+      color: #4E71FE;
+    }
+  `}
+  
+`
---
+++ src/components/content.tsx
@@ -0,0 +1,8 @@
+export const content = {
+    id: '1',
+    card: {
+      title: 'Headline',
+      text: 'Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.',
+      image: 'https://s3-alpha-sig.figma.com/img/295b/9b3e/0cbd17e4381497547009ce81acc4eee3?Expires=1714348800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TffTGaFCAOOwDs8B9U7UzjJMNmHs5GZQR-egKn3ti0oM994pfJG4xCs2a-jDpo83DI7aF7ZF0KLC9gf6gcKBpb8IyXPXPZeC6AmjhFs-zM8b3N5Jk6v1fNevWsrYaQ3iBs8b7PiziWrUFXO7nVEGFU9o-~dpCOosAHyH9eBzKexI0gAGVFXfxgqaH8CVqytOakZJhIG5QaICv~31AcecieOJJIVFGWJWcszuBMLzwRPIeiETKTGYE4QKB0jk2Ykb-chcsCK~1j4Oy0i9FqolNy9uBVWWkW~aN1olenqCWDopK39BsFGa4rLUmsN5Vu735s0xCSs7cJPKQ2cwGwz~Eg__',
+    }
+}
